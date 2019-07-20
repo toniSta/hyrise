ddc7f7ef2f194cfcb98aee4e4354731e0b1c516e
Toni Stachewicz <toni.stachewicz@student.hpi.de>
2019-07-20 15:13:42 +0200

    lineitem limited to 1024 lines

--- src/bin/server.cpp
+++ src/bin/server.cpp
@@ -12,6 +12,13 @@
 #include "utils/load_table.hpp"
 #include "tpch/tpch_table_generator.hpp"
 
+
+
+#include "sql/sql_pipeline_builder.hpp"
+#include "sql/sql_pipeline.hpp"
+#include "sql/sql_pipeline_statement.hpp"
+#include "sql/sql_translator.hpp"
+
 int main(int argc, char* argv[]) {
   uint16_t port = 5432;
 
@@ -33,6 +40,10 @@ int main(int argc, char* argv[]) {
   // constructor and then runs forever.
   opossum::Server server{io_service, port};
   opossum::TpchTableGenerator{0.01f, 100'000}.generate_and_store();
+     const std::string sql = "CREATE TABLE lineitem1024 (L_ORDERKEY INTEGER NOT NULL,L_PARTKEY INTEGER NOT NULL,L_SUPPKEY INTEGER NOT NULL,L_LINENUMBER INTEGER,L_QUANTITY float,L_EXTENDEDPRICE float,L_DISCOUNT float,L_TAX float,L_RETURNFLAG VARCHAR(1),L_LINESTATUS VARCHAR(1),L_SHIPDATE VARCHAR(10),L_COMMITDATE VARCHAR(10),L_RECEIPTDATE VARCHAR(10),L_SHIPINSTRUCT VARCHAR(25),L_SHIPMODE VARCHAR(10),L_COMMENT VARCHAR(44)); insert into lineitem1024 select top 1024 * from lineitem;";
+
+   auto sql_pipeline = std::make_shared<opossum::SQLPipeline>(opossum::SQLPipelineBuilder{sql}.create_pipeline());
+  sql_pipeline->get_result_table();
   io_service.run();
 
   return 0;
